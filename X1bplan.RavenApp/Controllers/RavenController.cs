db38f42d031347d204820807f7cb41fab6a7010c
Thomas Swann <ts.swann@gmail.com>
2013-03-24 21:00:10 +0000

    Adding code to initialise DocumentStore and base controller to manage sessions

---
+++ X1bplan.RavenApp/Controllers/RavenController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Raven.Client;
+
+namespace X1bplan.RavenApp.Controllers
+{
+    public abstract class RavenController : Controller
+    {
+        public static IDocumentStore DocumentStore { get; set; }
+
+        public IDocumentSession RavenSession { get; protected set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            RavenSession = MvcApplication.Store.OpenSession();
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.IsChildAction)
+                return;
+
+            using (RavenSession)
+            {
+                if (filterContext.Exception != null)
+                    return;
+
+                if (RavenSession != null)
+                    RavenSession.SaveChanges();
+            }
+        }
+    }
+}
--- X1bplan.RavenApp/Global.asax.cs
+++ X1bplan.RavenApp/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Indexes;
+using X1bplan.RavenApp.Controllers;
 
 namespace X1bplan.RavenApp
 {
@@ -12,6 +14,30 @@ namespace X1bplan.RavenApp
     // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static DocumentStore Store;
+
+        public MvcApplication()
+        {
+            BeginRequest += (sender, args) =>
+            {
+                HttpContext.Current.Items["CurrentRequestRavenSession"] = RavenController.DocumentStore.OpenSession();
+            };
+
+            EndRequest += (sender, args) =>
+            {
+                using (var session = (IDocumentSession)HttpContext.Current.Items["CurrentRequestRavenSession"])
+                {
+                    if (session == null)
+                        return;
+
+                    if (Server.GetLastError() != null)
+                        return;
+
+                    session.SaveChanges();
+                }
+            };
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,6 +45,19 @@ namespace X1bplan.RavenApp
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            InitialiseDocumentStore();
+
+            RavenController.DocumentStore = Store;
+        }
+
+        private void InitialiseDocumentStore()
+        {
+            if (Store != null) return;
+
+            Store = new DocumentStore { ConnectionStringName = "RavenDb", DefaultDatabase = "RavenApp" };
+            Store.Initialize();
+            IndexCreation.CreateIndexes(Assembly.Load("X1bplan.Indexes"), Store);
         }
     }
 }
